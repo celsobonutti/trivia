e9829391e7854fc00ccb9008f16bb8a8406261c6
Celso Bonutti Filho <celso.bonuttif@gmail.com>
2020-09-05 17:47:29 -0300

    Tests API call

--- src/utils/__tests__/fetchQuestions.test.ts
+++ src/utils/__tests__/fetchQuestions.test.ts
@@ -1,9 +1,12 @@
-import questions from '../../test_helpers/response_sample.json';
+import responseSample from '../../test_helpers/response_sample.json';
+import fetchMock from 'jest-fetch-mock';
 
 import {
   makeQueryString,
   parseQuestion,
-  parseQuestionText
+  parseQuestions,
+  parseQuestionText,
+  fetchQuestions
 } from '../fetchQuestions';
 
 describe('it generates query strings correctly', () => {
@@ -47,6 +50,8 @@ describe('it generates query strings correctly', () => {
 });
 
 describe('it parses questions correctly', () => {
+  const jsonQuestions = responseSample.results;
+
   test('when its text has double quotes', () => {
     const question = 'Do you like &quot;memes&quot;?';
 
@@ -71,5 +76,30 @@ describe('it parses questions correctly', () => {
     expect(parsedQuestion).toBe('Do you like "memes\'?');
   });
 
-  test('when parsing a full body', () => {});
+  test('when parsing a full body', () => {
+    const question = parseQuestion(jsonQuestions[0]);
+
+    expect(question.label).toBe(
+      'Pac-Man was invented by the designer Toru Iwatani while he was eating pizza.'
+    );
+    expect(question.difficulty).toBe('easy');
+    expect(question.answer).toBe(true);
+    expect(question.category).toBe('Entertainment: Video Games');
+  });
+
+  test('when parsing a group of questions', () => {
+    const questions = parseQuestions(jsonQuestions);
+
+    questions.forEach((question, index) => {
+      expect(question).toStrictEqual(parseQuestion(jsonQuestions[index]));
+    });
+  });
+
+  test('when fetching from the API', async () => {
+    fetchMock.mockResponseOnce(JSON.stringify(responseSample));
+
+    fetchQuestions({}).then((data) => {
+      expect(data).toStrictEqual(parseQuestions(jsonQuestions));
+    });
+  });
 });
